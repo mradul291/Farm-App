6a5bfb0caad115da0f7d3c1b85ef89bfb6da27e8
mradul291 <mishramradul29@gmail.com>
2025-07-07 16:30:23 +0530

    Test

--- farmer/farmer/doctype/store_transfer/store_transfer.js
+++ farmer/farmer/doctype/store_transfer/store_transfer.js
@@ -1,8 +1,22 @@
-// Copyright (c) 2025, chirag and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Store Transfer", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Store Transfer', {
+    item(frm) {
+        if (frm.doc.item) {
+            frappe.call({
+                method: "frappe.client.get_list",
+                args: {
+                    doctype: "Item Default",
+                    filters: {
+                        parent: frm.doc.item
+                    },
+                    fields: ["default_warehouse"],
+                    limit_page_length: 1
+                },
+                callback(r) {
+                    if (r.message && r.message.length > 0) {
+                        frm.set_value("from_warehouse", r.message[0].default_warehouse);
+                    }
+                }
+            });
+        }
+    }
+});
